cacfd6faf7f43ee881cb056f725b4a221f6721b8
Adrián García Masip <upvadrian@gmail.com>
2023-09-30 12:13:58 +0200

    Añadido el public

--- list.h
+++ list.h
@@ -1,13 +1,14 @@
 template <typename T>
 
 class list{
-	void insert(int pos, T e) = 0;
-	void append(T e);
-	void prepend(T e) = 0;
-	T remove(int pos) = 0;
-	T get(int pos) = 0;
-	int search(T e) = 0;
-	bool empty() = 0;
-	int size() = 0;
+	public:
+		void insert(int pos, T e) = 0;
+		void append(T e);
+		void prepend(T e) = 0;
+		T remove(int pos) = 0;
+		T get(int pos) = 0;
+		int search(T e) = 0;
+		bool empty() = 0;
+		int size() = 0;
 
 };
